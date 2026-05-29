fd8af80614832d6925cf807abaadcd4476dbf631
seo-er <tjguswjd32@gmail.com>
2026-05-29 18:16:07 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -572,7 +572,27 @@ return (
     key={r[0]}
     className="grid grid-cols-6 px-8 py-5 border-b border-[#F2F4F6] items-center hover:bg-[#FAFBFC] transition"
   >
-    <span>{r[0]}</span>
+    <button
+  onClick={() => navigator.clipboard.writeText(r[0])}
+  className="
+    inline-flex
+    items-center
+    gap-2
+    w-fit
+    px-3
+    py-1.5
+    rounded-md
+    bg-[#F2F4F6]
+    hover:bg-[#E5E8EB]
+    transition
+    font-mono
+    text-[14px]
+    text-[#4E5968]
+  "
+>
+  <span>{r[0]}</span>
+  <span className="text-[#8B95A1]">⧉</span>
+</button>
     <span>{r[1]}</span>
     <span>{r[2]}</span>
     <span>{r[3]}</span>
@@ -833,27 +853,7 @@ return (
       key={r[0]}
       className="grid grid-cols-5 px-8 py-5 border-b border-[#F2F4F6] items-center hover:bg-[#FAFBFC] transition"
     >
-     <button
-  onClick={() => navigator.clipboard.writeText(r[0])}
-  className="
-    inline-flex
-    items-center
-    gap-2
-    w-fit
-    px-3
-    py-1.5
-    rounded-md
-    bg-[#F2F4F6]
-    hover:bg-[#E5E8EB]
-    transition
-    font-mono
-    text-[14px]
-    text-[#4E5968]
-  "
->
-  <span>{r[0]}</span>
-  <span className="text-[#8B95A1]">⧉</span>
-</button>
+      <span>{r[0]}</span>
       <span>{r[1]}</span>
       <span>{r[2]}</span>
       <span>{r[3]}</span>
